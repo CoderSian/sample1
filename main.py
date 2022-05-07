5188abdac2c21a40faa6076887be8a4e5d010be2
CoderSian <dyddlydb@naver.com>
2022-05-08 00:29:39 +0900

    master

---
+++ main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+#드라이버 초기화
+driver = webdriver.Chrome("/Users/a1234/Documents/coding/python/sample1/chromedriver")
+#URL 얻기
+driver.get("http://blog.naver.com/lynn1128")
